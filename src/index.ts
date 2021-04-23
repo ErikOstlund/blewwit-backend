6f79a0aae0953ef8eb41399d3bb57b8690399215
ErikOstlund <eostlund74@gmail.com>
2021-04-23 16:58:49 -0500

    MikroORM set-up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hey there, Blewwit!');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+	// connect to db
+	const orm = await MikroORM.init(microConfig);
+
+	// run migrations
+	await orm.getMigrator().up();
+
+	// run SQL
+	// const post = orm.em.create(Post, { title: 'first test post!' });
+	// await orm.em.persistAndFlush(post);
+
+	// get all posts
+	const posts = await orm.em.find(Post, {});
+	console.log(posts);
+};
+
+main().catch((err) => {
+	console.error(err);
+});
