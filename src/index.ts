351947754fda69978ce02afb3acc663dd1b979a2
ErikOstlund <eostlund74@gmail.com>
2021-04-23 17:50:48 -0500

    Got Express server running

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
 	// connect to db
 	const orm = await MikroORM.init(microConfig);
-
 	// run migrations
 	await orm.getMigrator().up();
 
-	// run SQL
-	// const post = orm.em.create(Post, { title: 'first test post!' });
-	// await orm.em.persistAndFlush(post);
-
-	// get all posts
-	const posts = await orm.em.find(Post, {});
-	console.log(posts);
+	const app = express();
+	app.get('/', (_, res) => {
+		res.send('hello!');
+	});
+	app.listen(4000, () => {
+		console.log('server started on localhost:4000');
+	});
 };
 
 main().catch((err) => {
