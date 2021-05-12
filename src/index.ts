86c72d92d24bf86f1bb05b12caf2048a6a2e294a
ErikOstlund <eostlund74@gmail.com>
2021-05-12 15:24:22 -0500

    Post Resolver

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
@@ -6,6 +7,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
 	// connect to db
@@ -19,9 +21,10 @@ const main = async () => {
 	// create graphql endpoint (on our express server)
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [HelloResolver],
+			resolvers: [HelloResolver, PostResolver],
 			validate: false
-		})
+		}),
+		context: () => ({ em: orm.em })
 	});
 
 	apolloServer.applyMiddleware({ app });
